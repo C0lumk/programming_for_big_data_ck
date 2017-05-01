2b86f9b1fd9afefe6ed30c29c27427ac86cfd9fd
Colum Kenny <Colum Kenny>
2017-05-01 15:26:53 +0100

    added user functionality to calculator

--- CA05/Calculator.R
+++ CA05/Calculator.R
@@ -13,8 +13,13 @@ multiply <- function(x, y) {
 }
 
 divide <- function(x, y) {
-  
-  return(x / y)
+  if (x == 0) {
+    return ("undefined")
+  }
+  else if (y == 0) {
+    return("undefined")
+  }
+  else return(x / y)
 }
 
 exponent <- function(x, y) {
@@ -51,13 +56,20 @@ print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Exponent")
+print("6.Cube")
+print("7.Square")
+print("8.Exponential")
+print("9.RectanglePerimeter")
+print("10.Pythagorus")
+print("11.Sine")
 
-choice = as.integer(readline(prompt="Enter choice[1/2/3/4]: "))
+choice = as.integer(readline(prompt="Enter choice: "))
 
 num1 = as.integer(readline(prompt="Enter first number: "))
 num2 = as.integer(readline(prompt="Enter second number: "))
 
-operator <- switch(choice,"+","-","*","/")
-result <- switch(choice, add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2))
+operator <- switch(choice,"add","subtract","multiply","divide","exponent","cube","square","exponential","rectanglePerimeter","pythagorus","sine")
+result <- switch(choice, add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2), exponent(num1, num2), cube(num1), square(num1), exponential(num1),rectanglePerimeter(num1, num2),pythagorus(num1, num2), sin(num1, num2))
 
-print(paste(num1, operator, num2, "=", result))
+print(paste("First number:",num1,"Second number:", num2, "Operator:",operator, "Result:", result))
